da969a2febcbf290e3be9db21f648449b41a6fac
Suyash Thakur <64472769+suyash0908@users.noreply.github.com>
2020-08-04 00:30:01 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,13 +5,13 @@ s=smtplib.SMTP('smtp.gmail.com',587)
 
 s.starttls()
 
-s.login("anand31798@gmail.com","3179724774937007")
+s.login("thakur2suyash@gmail.com","3179724774937007")
 
 subject1='Important'
 
 
 message1 = "your latest code ha been failed, please debug it as soon as possible."
 
-s.sendmail("anand31798@gmail.com","1706298@kiit.ac.in",message1)
+s.sendmail("thakur2suyash@gmail.com","1706277@kiit.ac.in",message1)
 
 s.quit()
